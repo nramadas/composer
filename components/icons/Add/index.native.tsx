af12b7749f4d79b3f413a05972dcc63240306fc9
Niranjan Ramadas <mail@niranjan.me>
2021-06-20 11:44:21 +0530

    Add some icons

---
+++ components/icons/Add/index.native.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Svg, Path, SvgProps } from 'react-native-svg';
+
+export function Add(props: SvgProps) {
+  return (
+    <Svg {...props} viewBox="0 0 28 28">
+      <Path d="M24 13H15V4C15 3.447 14.553 3 14 3C13.447 3 13 3.447 13 4V13H4C3.447 13 3 13.447 3 14C3 14.553 3.447 15 4 15H13V24C13 24.553 13.447 25 14 25C14.553 25 15 24.553 15 24V15H24C24.553 15 25 14.553 25 14C25 13.447 24.553 13 24 13Z" />
+    </Svg>
+  );
+}
---
+++ components/icons/Add/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Add(props: React.SVGAttributes<SVGElement>) {
+  return (
+    <svg {...props} viewBox="0 0 28 28">
+      <path d="M24 13H15V4C15 3.447 14.553 3 14 3C13.447 3 13 3.447 13 4V13H4C3.447 13 3 13.447 3 14C3 14.553 3.447 15 4 15H13V24C13 24.553 13.447 25 14 25C14.553 25 15 24.553 15 24V15H24C24.553 15 25 14.553 25 14C25 13.447 24.553 13 24 13Z" />
+    </svg>
+  );
+}
