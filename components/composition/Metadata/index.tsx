bdcc1f84701f16aad472c46ec802b9e682f806be
Niranjan Ramadas <mail@niranjan.me>
2021-07-03 13:46:18 +0530

    Display the raaga in metadata

--- components/composition/Metadata/index.tsx
+++ components/composition/Metadata/index.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 
 import { Composer } from '#components/composition/Composer';
 import { Raaga } from '#components/composition/Raaga';
+import { Scale } from '#components/composition/Scale';
 import { Body1 } from '#components/typography/Body1';
+import { useSelector } from '#lib/hooks/useSelector';
 
 import styles from './index.module.scss';
 
@@ -12,6 +14,8 @@ interface Props {
 }
 
 export function Metadata(props: Props) {
+  const raaga = useSelector(state => state.composition.raaga);
+
   return (
     <div className={cx(styles.container, props.className)}>
       <div className={styles.label}>
@@ -19,9 +23,21 @@ export function Metadata(props: Props) {
       </div>
       <Composer />
       <div className={styles.label}>
-        <Body1>Raaga: </Body1>
+        <Body1>Rāga: </Body1>
       </div>
       <Raaga />
+      {raaga !== 'RaagaMaalikaa' && (
+        <>
+          <div className={styles.label}>
+            <Body1>Ārohanam: </Body1>
+          </div>
+          <Scale className={styles.scale} raaga={raaga} style="aa" />
+          <div className={styles.label}>
+            <Body1>Avarohanam: </Body1>
+          </div>
+          <Scale className={styles.scale} raaga={raaga} style="av" />
+        </>
+      )}
     </div>
   );
 }
