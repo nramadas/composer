76b0934518192e0eb646b749f8f4c8f46ca6ae2e
Niranjan Ramadas <mail@niranjan.me>
2021-07-09 17:23:37 +0530

    Hide extra rows in composition view mode

--- components/viewComposition/View/index.tsx
+++ components/viewComposition/View/index.tsx
@@ -6,11 +6,36 @@ import { H3 } from '#components/typography/H3';
 import { H4 } from '#components/typography/H4';
 import { Metadata } from '#components/viewComposition/Metadata';
 import { Row } from '#components/viewComposition/Row';
+import { BlockType } from '#lib/models/Block';
 import { Composition } from '#lib/models/Composition';
 import { User } from '#lib/models/User';
 
 import fetchCompositionQuery from './fetchComposition.gql';
 import styles from './index.module.scss';
+
+function cleanUp(blocks: Composition['blocks']): Composition['blocks'] {
+  const result: Composition['blocks'] = [];
+  let skipCleanup = false;
+
+  for (let i = blocks.length - 1; i >= 0; i--) {
+    const row = blocks[i];
+    if (skipCleanup) {
+      result.push(row);
+    } else {
+      row.forEach(block => {
+        if (block.type !== BlockType.Undefined) {
+          skipCleanup = true;
+        }
+      });
+
+      if (skipCleanup) {
+        result.push(row);
+      }
+    }
+  }
+
+  return result.reverse();
+}
 
 interface Props {
   className?: string;
@@ -44,7 +69,7 @@ export function View(props: Props) {
       <Metadata className={styles.metaData} composition={composition} />
       <div className={styles.blocksWrapper}>
         <div className={cx(styles.blocks)}>
-          {composition.blocks.map((row, i) => (
+          {cleanUp(composition.blocks).map((row, i) => (
             <Row
               blocks={row}
               className={styles.row}
