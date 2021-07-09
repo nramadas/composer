735e9063fe5ad447e876c2a6a951f8735c91d314
Niranjan Ramadas <mail@niranjan.me>
2021-07-09 12:59:56 +0530

    Adjust landing page

--- components/eyecandy/Hero/index.tsx
+++ components/eyecandy/Hero/index.tsx
@@ -1,8 +1,10 @@
 import cx from 'classnames';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useQuery } from 'urql';
 
 import { Filled } from '#components/controls/buttons/flat/Filled';
+import fetchUserQuery from '#components/controls/GlobalHeader/Account/fetchUser.gql';
 import { Ganapathi } from '#components/eyecandy/Ganapathi';
 import { H1 } from '#components/typography/H1';
 import { H3 } from '#components/typography/H3';
@@ -15,6 +17,8 @@ interface Props {
 }
 
 export function Hero(props: Props) {
+  const [result] = useQuery({ query: fetchUserQuery });
+
   return (
     <article className={cx(props.className, styles.container)}>
       <div className={styles.image}>
@@ -24,7 +28,9 @@ export function Hero(props: Props) {
         <H1 className={styles.title}>Ganapathi</H1>
         <H3 className={styles.subTitle}>Carnatic Music Notation</H3>
         <Link className={styles.cta} to={PageRoute.Compose}>
-          <Filled>Try it for free</Filled>
+          <Filled>
+            {result.data?.me ? 'Go to editor' : 'Try it for free'}
+          </Filled>
         </Link>
       </div>
     </article>
